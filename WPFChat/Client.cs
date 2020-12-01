cc045981e6f49af6df99de040984d0f970c2ab9f
alex <jrmlelantus@gmail.com>
2020-12-01 22:47:23 +0300

    тесты сокета
    пробовал понять как работает эта поебота

--- WPFChat/Client.cs
+++ WPFChat/Client.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,5 +16,18 @@ namespace WPFChat
         //Для особенных
         //Заебал
         //sam zaebal
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private void Main(string[] args)
+        {
+
+           
+
+
+            socket.Connect("127.0.0.1",8080);
+
+            Console.ReadLine();
+            
+        }
     }
+    
 }
