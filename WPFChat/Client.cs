bdcc7f1805bf9b1bfbf593704ae2d587e3c71e75
alex <jrmlelantus@gmail.com>
2020-12-01 22:48:30 +0300

    Revert "тесты сокета"
    This reverts commit cc045981e6f49af6df99de040984d0f970c2ab9f.

--- WPFChat/Client.cs
+++ WPFChat/Client.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,18 +14,5 @@ namespace WPFChat
         //Для особенных
         //Заебал
         //sam zaebal
-        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        private void Main(string[] args)
-        {
-
-           
-
-
-            socket.Connect("127.0.0.1",8080);
-
-            Console.ReadLine();
-            
-        }
     }
-    
 }
